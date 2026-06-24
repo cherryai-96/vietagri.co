57f86dc0da472e4bf0cdd7d98c12478957c8ab42
SHUU <tt@192.168.1.6>
2026-06-24 09:03:28 +0700

    Add Products & Solutions page and translations

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { Header } from './components/Header';
 import { Footer } from './components/Footer';
 import { Home } from './pages/Home';
 import { About } from './pages/About';
+import { Products } from './pages/Products';
 import { Services } from './pages/Services';
 import { VietWolffia } from './pages/VietWolffia';
 import { Sustainability } from './pages/Sustainability';
@@ -38,6 +39,7 @@ function App() {
                     <Routes>
                       <Route path="/" element={<Home />} />
                       <Route path="/about" element={<About />} />
+                      <Route path="/products" element={<Products />} />
                       <Route path="/services" element={<Services />} />
                       <Route path="/viet-wolffia" element={<VietWolffia />} />
                       <Route path="/sustainability" element={<Sustainability />} />
